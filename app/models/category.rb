2c2ba2e6033d93a98c4bb62320a2a4326c215476
Alireza Bashiri <azbshiri@gmail.com>
2018-06-21 01:55:10 +0430

    Remove hashid from category
    Why?
    I thought categories would be safe even if their actual ids are public.
    Also hashid  gem doesn't support find by array of ids which force us to loop through each id and find it
    and it's a performance issue.

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,4 @@
 class Category < ApplicationRecord
-  include Hashid::Rails
-
   belongs_to :user
   has_many :pets
   validates :name, uniqueness: { scope: :user_id }
--- app/serializers/category_serializer.rb
+++ app/serializers/category_serializer.rb
@@ -2,8 +2,4 @@ class CategorySerializer < ActiveModel::Serializer
   belongs_to :user
   has_many :pets
   attributes :id, :name, :created_at, :updated_at
-
-  def id
-    object.hashid
-  end
 end
