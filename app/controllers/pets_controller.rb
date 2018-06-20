1d1f9105a4d3ae9011c983acc7345e1820ddcb18
Alireza Bashiri <azbshiri@gmail.com>
2018-06-20 16:44:21 +0430

    Add view pet API
    What?
    Added a test helper called `json_response` to assert json respones.

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -2,4 +2,8 @@ class PetsController < ApplicationController
   def index
     render json: Pet.includes(:user).all
   end
+
+  def show
+    render json: Pet.includes(:user).find(params[:id])
+  end
 end
--- test/controllers/pets_controller_test.rb
+++ test/controllers/pets_controller_test.rb
@@ -5,4 +5,24 @@ class PetsControllerTest < ActionDispatch::IntegrationTest
     get pets_url
     assert_response :ok
   end
+
+  test "all users can view pet details without authentication" do
+    pet_owner = User.create!(
+        email: "test@example.com",
+        password: "123456@",
+        password_confirmation: "123456@",
+        role: "pet_owner"
+    )
+
+    pet = Pet.create!(
+        name: "foo",
+        pet_type: "dog",
+        user: @pet_owner
+    )
+
+    get url_for(pet)
+
+    assert_equal "foo", json_response["name"]
+    assert_response :ok
+  end
 end
