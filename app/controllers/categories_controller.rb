749bf920e3b26d16c893814d2954823d4ff5dbbf
Alireza Bashiri <azbshiri@gmail.com>
2018-06-21 01:23:44 +0430

    Add view categories API

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,2 +1,5 @@
 class CategoriesController < ApplicationController
+  def index
+    render json: Category.includes(:user).all
+  end
 end
--- test/controllers/categories_controller_test.rb
+++ test/controllers/categories_controller_test.rb
@@ -1,4 +1,8 @@
 require 'test_helper'
 
 class CategoriesControllerTest < ActionDispatch::IntegrationTest
+  test "all users can view categories without authentication" do
+    get categories_url
+    assert_response :ok
+  end
 end
