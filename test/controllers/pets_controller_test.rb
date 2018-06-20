bb7448debaf7bc0b055a15156ab0d818d4e5d810
Alireza Bashiri <azbshiri@gmail.com>
2018-06-21 00:21:20 +0430

    Setup users with related roles

--- test/controllers/pets_controller_test.rb
+++ test/controllers/pets_controller_test.rb
@@ -3,8 +3,24 @@ require 'test_helper'
 class PetsControllerTest < ActionDispatch::IntegrationTest
   def setup
     @password = "1234567@"
-    @user1 = User.create!(email: "test+1@gmail.com", password: @password, password_confirmation: @password)
-    @user2 = User.create!(email: "test+2@gmail.com", password: @password, password_confirmation: @password)
+    @customer = User.create!(
+        role: :customer,
+        email: "test+1@gmail.com",
+        password: @password,
+        password_confirmation: @password
+    )
+    @pet_owner = User.create!(
+        role: :pet_owner,
+        email: "test+2@gmail.com",
+        password: @password,
+        password_confirmation: @password
+    )
+    @manager = User.create!(
+        role: :manager,
+        email: "test+3@gmail.com",
+        password: @password,
+        password_confirmation: @password
+    )
   end
 
   test "all users can view pets without authentication" do
@@ -16,7 +32,7 @@ class PetsControllerTest < ActionDispatch::IntegrationTest
     pet = Pet.create!(
         name: "foo",
         pet_type: "dog",
-        user: @user1
+        user: @pet_owner
     )
 
     get url_for(pet)
@@ -31,28 +47,34 @@ class PetsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "users cannot create pets without name" do
-    post pets_url, params: { pet_type: "dog" }, headers: login_basic(@user1.email, @password)
+    post pets_url, params: { pet_type: "dog" }, headers: login_basic(@pet_owner.email, @password)
     assert_response :bad_request
   end
 
   test "users cannot create pets with duplicate in their own scope name" do
-    post pets_url, params: { name: "foo", pet_type: "dog" }, headers: login_basic(@user1.email, @password)
+    post pets_url, params: { name: "foo", pet_type: "dog" }, headers: login_basic(@pet_owner.email, @password)
     assert_response :created
 
-    post pets_url, params: { name: "foo", pet_type: "dog" }, headers: login_basic(@user2.email, @password)
+    post pets_url, params: { name: "foo", pet_type: "dog" }, headers: login_basic(@manager.email, @password)
     assert_response :created
 
-    post pets_url, params: { name: "foo", pet_type: "dog" }, headers: login_basic(@user1.email, @password)
+    post pets_url, params: { name: "foo", pet_type: "dog" }, headers: login_basic(@pet_owner.email, @password)
     assert_response :bad_request
   end
 
   test "users cannot create pets without pet_type" do
-    post pets_url, params: { name: "foo" }, headers: login_basic(@user1.email, @password)
+    post pets_url, params: { name: "foo" }, headers: login_basic(@pet_owner.email, @password)
     assert_response :bad_request
   end
 
   test "users cannot create pets with invalid pet_type" do
-    post pets_url, params: { name: "foo", pet_type: "invalid" }, headers: login_basic(@user1.email, @password)
+    post pets_url, params: { name: "foo", pet_type: "invalid" }, headers: login_basic(@pet_owner.email, @password)
     assert_response :bad_request
   end
+
+  test "customers cannot create pets" do
+    post pets_url, params: { name: "bar", pet_type: "dog"}, headers: login_basic(@customer.email, @password)
+    assert_response :forbidden
+  end
+
 end
