e16fd046858abac25568a68cb2a20222bf74ed54
Alireza Bashiri <azbshiri@gmail.com>
2018-06-21 03:38:59 +0430

    Expose pets database sequential ids
    Why?
    Due to performance issue when finding multiple pets to add a category to.

--- app/serializers/pet_serializer.rb
+++ app/serializers/pet_serializer.rb
@@ -2,10 +2,6 @@ class PetSerializer < ActiveModel::Serializer
   belongs_to :owner
   attributes :id, :name, :pet_type, :created_at, :updated_at
 
-  def id
-    object.hashid
-  end
-
   def owner
     object.user
   end
