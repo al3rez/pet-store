cd5d527d86a48f8c9c46e3d568906c4786e51ef4
Alireza Bashiri <azbshiri@gmail.com>
2018-06-20 16:42:59 +0430

    Add missing `all` to test description

--- test/controllers/pets_controller_test.rb
+++ test/controllers/pets_controller_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 
 class PetsControllerTest < ActionDispatch::IntegrationTest
-  test "users can view pets without authentication" do
+  test "all users can view pets without authentication" do
     get pets_url
     assert_response :ok
   end
